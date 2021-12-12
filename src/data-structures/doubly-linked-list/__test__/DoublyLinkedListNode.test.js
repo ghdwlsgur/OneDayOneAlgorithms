73ac9a0348f66285d3ac613e514279b866f0d7e8
HongJinHyeok <redmax45@naver.com>
2021-12-12 13:35:38 +0900

    이중연결리스트

--- src/data-structures/doubly-linked-list/__test__/DoublyLinkedListNode.test.js
+++ src/data-structures/doubly-linked-list/__test__/DoublyLinkedListNode.test.js
@@ -12,7 +12,6 @@ describe('DoublyLinkedListNode', () => {
   it('should create list node with object as a value', () => {
     const nodeValue = { value: 1, key: 'test' };
     const node = new DoublyLinkedListNode(nodeValue);
-
     expect(node.value.value).toBe(1);
     expect(node.value.key).toBe('test');
     expect(node.next).toBeNull();
