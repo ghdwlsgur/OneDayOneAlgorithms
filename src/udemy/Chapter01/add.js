578bd37054ca55d34b353b15f433f02d189091cc
HongJinHyeok <redmax45@naver.com>
2022-03-27 15:45:37 +0900

    시간복잡도 테스트

--- src/udemy/Chapter01/add.js
+++ src/udemy/Chapter01/add.js
@@ -12,3 +12,19 @@ console.log(addUpTo(1000000000));
 const after = new Date();
 const t2 = after.getTime();
 console.log(`Time Elapsed: ${(t2 - t1) / 1000} seconds.`);
+
+function logAtLeast5(n) {
+  for (var i = 1; i <= Math.max(5, n); i++) {
+    console.log(i);
+  }
+}
+
+logAtLeast5(3);
+
+function logAtMost5(n) {
+  for (var i = 1; i <= Math.min(5, n); i++) {
+    console.log(i);
+  }
+}
+
+logAtMost5(30);
