d3f809fe829eeab12a2a7494125e3355402bd5b4
HongJinHyeok <redmax45@naver.com>
2022-03-29 11:24:19 +0900

    📝 문자 세기 알고리즘

--- src/udemy/Chapter02/index.js
+++ src/udemy/Chapter02/index.js
@@ -34,5 +34,45 @@ function charCount(str) {
   return result;
 }
 
+function charCount2(str) {
+  const obj = {};
+  for (let i = 0; i < str.length; i++) {
+    let char = str[i].toLowerCase();
+    if (/[a-z0-9]/.test(char)) {
+      if (obj[char] > 0) obj[char]++;
+      else obj[char] = 1;
+    }
+  }
+  return obj;
+}
+
+function charCount3(str) {
+  const obj = {};
+  for (let char of str) {
+    if (isAlphanumeric(char)) {
+      char = char.toLowerCase();
+      obj[char] = ++obj[char] || 1;
+    }
+  }
+  return obj;
+}
+
+function isAlphanumeric(char) {
+  let code = char.charCodeAt(0);
+  if (
+    !(code > 47 && code < 58) &&
+    !(code > 64 && code < 91) &&
+    !(code > 96 && code < 123)
+  ) {
+    return false;
+  }
+  return true;
+}
 console.log(charCount('hello'));
 console.log(charCount('Hi there!'));
+console.log(charCount2('Hi there!'));
+console.log(charCount3('!!!hi   my name '));
+
+// nemeric (0-9) charCodeAt() 47 ~ 58
+// upper alpha (A-Z) 64 ~ 91
+// lower alpha (a-z) 96 ~ 123
