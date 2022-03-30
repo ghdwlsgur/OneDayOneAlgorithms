19ee953eb6e9636b6aa984b0c9f53c544dceac5f
HongJinHyeok <redmax45@naver.com>
2022-03-30 15:41:09 +0900

    ✏️ udemy code

---
+++ src/udemy/Chapter03/index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+function same(arr1, arr2) {
+  if (arr1.length !== arr2.length) return false;
+
+  for (let i = 0; i < arr1.length; i++) {
+    let correctIndex = arr2.indexOf(arr1[i] ** 2);
+    if (correctIndex === -1) return false;
+    arr2.splice(correctIndex, 1);
+  }
+  return true;
+}
+
+console.log(same([4, 4, 1], [1, 16, 16]));
