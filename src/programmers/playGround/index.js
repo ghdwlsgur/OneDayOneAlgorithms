eff6742be15d59a7346565389bf68b95a6113c37
HongJinHyeok <redmax45@naver.com>
2022-04-06 15:27:51 +0900

    ⚡️ 짝문자 제거하기

--- src/programmers/playGround/index.js
+++ src/programmers/playGround/index.js
@@ -110,3 +110,24 @@
 // }
 
 // console.log(solution([3, 1, 2, 3]));
+
+const solution = string => {
+  const charArray = string.split('');
+  if (charArray.length <= 1) return 0;
+
+  const deletePair = () => {
+    const startArray = [...charArray];
+    for (let i = 0; i < charArray.length; i++) {
+      if (charArray[i] === charArray[i + 1]) charArray.splice(i, 2);
+    }
+
+    if (startArray.length === charArray.length) return;
+    else if (startArray.length !== charArray.length) deletePair();
+  };
+
+  deletePair();
+
+  return charArray.length > 0 ? 0 : 1;
+};
+
+console.log(solution('aacsbbddeeggffggff'));
