75a3c038902c6a495df6edca64c21a07beb33b33
HongJinHyeok <redmax45@naver.com>
2022-04-10 01:53:26 +0900

    중복 제거

--- src/programmers/playGround/index.js
+++ src/programmers/playGround/index.js
@@ -1,13 +1,5 @@
-const solution = (array, commands) => {
-  const result = [];
-  commands.map(command => {
-    let newArray = [
-      ...array.slice(command[0] - 1, command[1]).sort((a, b) => a - b),
-    ];
-    result.push(newArray[command[2] - 1]);
-  });
-
-  return result;
+const solution = arr => {
+  return arr.filter((num, i) => num !== arr[i + 1]);
 };
 
-solution([1, 5, 2, 6, 3, 7, 4], [[2, 5, 3]]);
+console.log(solution([1, 1, 3, 3, 0, 1, 1]));
