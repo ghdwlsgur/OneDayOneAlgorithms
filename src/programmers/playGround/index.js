ce4e585f5e6c18bbc463170f241ac2c66d5a4179
HongJinHyeok <redmax45@naver.com>
2022-04-10 01:33:33 +0900

    array sorting

--- src/programmers/playGround/index.js
+++ src/programmers/playGround/index.js
@@ -1,57 +1,13 @@
-const solution = (id_list, report, k) => {
-  const reportObj = {};
-  const reportedObj = {};
-
-  let result = [];
-  for (let i = 0; i < id_list.length; i++) {
-    reportObj[id_list[i]] = [];
-    result[i] = 0;
-  }
-
-  report.filter(user => {
-    const reportUser = user.split(' ')[0];
-    const reportedUser = user.split(' ')[1];
-    reportedObj[reportedUser] = 0;
-    reportObj[reportUser].push(reportedUser);
-  });
-
-  Object.keys(reportObj).filter(user => {
-    reportObj[user] = [...new Set(reportObj[user])];
-    const reportedList = reportObj[user];
-    reportedList.filter(reportedUser => {
-      reportedObj[reportedUser] = ++reportedObj[reportedUser] || 1;
-    });
-  });
-
-  Object.values(reportObj).forEach((userList, userIndex) => {
-    for (let i = 0; i < userList.length; i++) {
-      if (reportedObj[userList[i]] >= k) result[userIndex] += 1;
-    }
-    if (userList.length === 0) result[userIndex] = 0;
+const solution = (array, commands) => {
+  const result = [];
+  commands.map(command => {
+    let newArray = [
+      ...array.slice(command[0] - 1, command[1]).sort((a, b) => a - b),
+    ];
+    result.push(newArray[command[2] - 1]);
   });
 
   return result;
 };
 
-console.log(
-  solution(
-    ['con', 'ryan'],
-    ['ryan con', 'ryan con', 'ryan con', 'ryan con'],
-    3,
-  ),
-);
-
-// console.log(
-//   solution(
-//     ['muzi', 'frodo', 'apeach', 'neo'],
-//     [
-//       'muzi frodo',
-//       'muzi frodo',
-//       'apeach frodo',
-//       'frodo neo',
-//       'muzi neo',
-//       'apeach muzi',
-//     ],
-//     3,
-//   ),
-// );
+solution([1, 5, 2, 6, 3, 7, 4], [[2, 5, 3]]);
